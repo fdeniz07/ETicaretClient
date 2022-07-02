1583b7f150fc1375acac70dd6d07c5e457db428e
Fatih Deniz <deniz.fatih@hotmail.de>
2022-07-03 01:59:59 +0200

    Fileupload -3

--- src/app/services/common/file-upload/file-upload.module.ts
+++ src/app/services/common/file-upload/file-upload.module.ts
@@ -1,13 +1,15 @@
 import { FileUploadComponent } from './file-upload.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgxFileDropModule } from 'ngx-file-drop';
 
 
 
 @NgModule({
   declarations: [FileUploadComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    NgxFileDropModule
   ]
 })
 export class FileUploadModule { }
