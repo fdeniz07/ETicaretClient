bec169a1db1cb1e4d282351ea2c8c38d06a331a3
Fatih Deniz <deniz.fatih@hotmail.de>
2022-08-24 12:11:26 +0200

    JWT Configuration

--- src/app/services/common/models/user.service.ts
+++ src/app/services/common/models/user.service.ts
@@ -3,7 +3,10 @@ import { Injectable } from '@angular/core';
 import { Create_User } from 'src/app/contracts/users/create_user';
 import { User } from 'src/app/entities/user';
 import { HttpClientService } from '../http-client.service';
-import { CustomToastrService } from '../../ui/custom-toastr.service';
+import { CustomToastrService, ToastrMessageType, ToastrPosition } from '../../ui/custom-toastr.service';
+import { Token } from 'src/app/contracts/token/token';
+import { TokenResponse } from 'src/app/contracts/token/tokenResponse';
+
 
 @Injectable({
   providedIn: 'root'
@@ -20,13 +23,25 @@ export class UserService {
     return await firstValueFrom(observable) as Create_User;
   }
 
-  async login(userNameOrEmail: string, password: string, callBackFunktion: () => void): Promise<void> {
-    const observable: Observable<any> = this.httpClientService.post({
+  async login(userNameOrEmail: string, password: string, callBackFunktion: () => void): Promise<any> {
+    const observable: Observable<any | TokenResponse> = this.httpClientService.post<any | TokenResponse>({
       controller: "users",
       action: "login"
     }, { userNameOrEmail, password })
 
-    await firstValueFrom(observable);
+    const tokenResponse: TokenResponse = await firstValueFrom(observable) as TokenResponse;
+    // debugger;
+    // console.log(tokenResponse);
+    // console.log(tokenResponse.token.accessToken);
+    if (tokenResponse) {
+      localStorage.setItem("accessToken", tokenResponse.token.accessToken);
+
+      this.toastrService.message("Kullanıcı girişi başarıyla sağlanmıştır.", "Giriş Başarılı", {
+        messageType: ToastrMessageType.Success,
+        position: ToastrPosition.TopRight
+      })
+    }
+
     callBackFunktion();
   }
 
